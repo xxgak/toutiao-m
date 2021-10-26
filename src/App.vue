e2ef3d16d0082c227a4e015f63f2e1dc4a995783
xxgak <1934245878@qq.com>
2021-10-26 15:33:12 +0800

    项目初始化，引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
-
+    <div>
+      <!-- 使用字体图标的时候，需要引入两个类名，一个是font-family的名字，一个字体图标的名字 -->
+      <i class="ttfont toutiao-dianzan"></i>
+    </div>
+    <div>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="info">信息按钮</van-button>
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+      <van-button type="danger">危险按钮</van-button>
+    </div>
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
     <!--  <router-view/>是路由的出口，这个标签一定不要删除 -->
     <router-view/>
   </div>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import store from './store'
 // 引入css和less文件的时候，是不需要起名字的（因为脚手架中有css预处理器less,所以我们可以直接用less写样式）
 import './styles/index.less'
 
+// 导入vant模块（vant的核心组件库）
+import Vant from 'vant'
+// 导入vant模块的css
+import 'vant/lib/index.css'
+// 注册vant模块（注册使用vant组件库）
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
